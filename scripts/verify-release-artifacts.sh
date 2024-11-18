9cc615af4add118537441a7357f936fb02f7ef86
Subhransu <47723536+Shubhranshu153@users.noreply.github.com>
2024-11-18 11:08:17 -0800

    fix: Add arm64 build and release (#99)
    Signed-off-by: Shubhranshu153 <mahapatra.shubhranshu@gmail.com>

--- scripts/verify-release-artifacts.sh
+++ scripts/verify-release-artifacts.sh
@@ -22,7 +22,14 @@ cur_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 dinch_daemon_project_root="$(cd -- "$cur_dir"/.. && pwd)"
 release_dir="${dinch_daemon_project_root}/release"
 
-arch="amd64"
+arch="${TARGET_ARCH:-}"
+if [ -z "$arch" ]; then
+    case $(uname -m) in
+        x86_64) arch="amd64" ;;
+        aarch64) arch="arm64" ;;
+        *) echo "Error: Unsupported arch $(uname -m)"; exit 1 ;;
+    esac
+fi
 
 function usage {
     echo "Usage: $0 <release_tag>"
