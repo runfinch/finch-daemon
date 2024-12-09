729e9700dce53b79a1ad3158be669491ac58246a
Arjun Raja Yogidas <arjunry@amazon.com>
2024-12-09 17:27:36 +0000

    chore: add ContainerIDFile options
    Signed-off-by: Arjun Raja Yogidas <arjunry@amazon.com>

--- api/handlers/container/create_test.go
+++ api/handlers/container/create_test.go
@@ -705,6 +705,27 @@ var _ = Describe("Container Create API ", func() {
 			Expect(rr.Body).Should(MatchJSON(jsonResponse))
 		})
 
+		It("should set ContainerIdFile option", func() {
+			body := []byte(`{
+				"Image": "test-image",
+				"HostConfig": {
+					"ContainerIDFile": "/lib/example.txt"
+				}
+			}`)
+			req, _ := http.NewRequest(http.MethodPost, "/containers/create", bytes.NewReader(body))
+
+			// expected create options
+			createOpt.CidFile = "/lib/example.txt"
+
+			service.EXPECT().Create(gomock.Any(), "test-image", nil, equalTo(createOpt), equalTo(netOpt)).Return(
+				cid, nil)
+
+			// handler should return success message with 201 status code.
+			h.create(rr, req)
+			Expect(rr).Should(HaveHTTPStatus(http.StatusCreated))
+			Expect(rr.Body).Should(MatchJSON(jsonResponse))
+		})
+
 		It("should return 404 if the image was not found", func() {
 			body := []byte(`{"Image": "test-image"}`)
 			req, _ := http.NewRequest(http.MethodPost, "/containers/create", bytes.NewReader(body))
