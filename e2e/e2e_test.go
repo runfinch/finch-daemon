eafcaac296f72fa04813053496d99c874b81ef70
Arjun Raja Yogidas <arjunry@amazon.com>
2025-04-09 15:27:38 +0000

    chore: update e2e_test.go
    Signed-off-by: Arjun Raja Yogidas <arjunry@amazon.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -5,7 +5,6 @@ package e2e
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"strings"
@@ -20,29 +19,36 @@ import (
 	"github.com/runfinch/finch-daemon/e2e/util"
 )
 
-// Subject defines which CLI the tests are run against, defaults to \"nerdctl\" in the user's PATH.
-var Subject = flag.String("subject", "nerdctl", `which CLI the tests are run against, defaults to "nerdctl" in the user's PATH.`)
-var SubjectPrefix = flag.String("daemon-context-subject-prefix", "", `A string which prefixes the command the tests are run against, defaults to "". This string will be split by spaces.`)
-var PrefixedSubjectEnv = flag.String("daemon-context-subject-env", "", `Environment to add when running a prefixed subject, in the form of a string like "EXAMPLE=foo EXAMPLE2=bar"`)
+const (
+	defaultNamespace   = "finch"
+	testE2EEnv         = "TEST_E2E"
+	middlewareE2EEnv   = "MIDDLEWARE_E2E"
+	opaTestDescription = "Finch Daemon OPA E2E Tests"
+	e2eTestDescription = "Finch Daemon Functional test"
+)
+
+var (
+	Subject            = flag.String("subject", "nerdctl", `which CLI the tests are run against, defaults to "nerdctl" in the user's PATH.`)
+	SubjectPrefix      = flag.String("daemon-context-subject-prefix", "", `A string which prefixes the command the tests are run against, defaults to "". This string will be split by spaces.`)
+	PrefixedSubjectEnv = flag.String("daemon-context-subject-env", "", `Environment to add when running a prefixed subject, in the form of a string like "EXAMPLE=foo EXAMPLE2=bar"`)
+)
 
 func TestRun(t *testing.T) {
-	if os.Getenv("MIDDLEWARE_E2E") == "1" {
+	switch {
+	case os.Getenv(middlewareE2EEnv) == "1":
 		runOPATests(t)
-	} else if os.Getenv("TEST_E2E") == "1" {
+	case os.Getenv(testE2EEnv) == "1":
 		runE2ETests(t)
-	} else {
+	default:
 		t.Skip("E2E tests skipped. Set TEST_E2E=1 to run regular E2E tests or MIDDLEWARE_E2E=1 to run OPA middleware tests")
 	}
 }
 
-func runOPATests(t *testing.T) {
-	if err := parseTestFlags(); err != nil {
-		log.Println("failed to parse go test flags", err)
-		os.Exit(1)
-	}
+func createTestOption() (*option.Option, error) {
+	return option.New([]string{*Subject, "--namespace", defaultNamespace})
+}
 
-	opt, _ := option.New([]string{*Subject, "--namespace", "finch"})
-
+func setupTestSuite(opt *option.Option) {
 	ginkgo.SynchronizedBeforeSuite(func() []byte {
 		tests.SetupLocalRegistry(opt)
 		return nil
@@ -50,93 +56,121 @@ func runOPATests(t *testing.T) {
 
 	ginkgo.SynchronizedAfterSuite(func() {
 		tests.CleanupLocalRegistry(opt)
-		// clean up everything after the local registry is cleaned up
 		command.RemoveAll(opt)
 	}, func() {})
+}
 
-	const description = "Finch Daemon OPA E2E Tests"
-	ginkgo.Describe(description, func() {
+func runOPATests(t *testing.T) {
+	if err := parseTestFlags(); err != nil {
+		log.Fatal("failed to parse go test flags:", err)
+	}
+
+	opt, err := createTestOption()
+	if err != nil {
+		log.Fatal("failed to create test option:", err)
+	}
+
+	setupTestSuite(opt)
+
+	ginkgo.Describe(opaTestDescription, func() {
 		tests.OpaMiddlewareTest(opt)
-		fmt.Print(opt)
 	})
 
-	gomega.RegisterFailHandler(ginkgo.Fail)
-	ginkgo.RunSpecs(t, description)
+	runTests(t, opaTestDescription)
 }
 
 func runE2ETests(t *testing.T) {
 	if err := parseTestFlags(); err != nil {
-		log.Println("failed to parse go test flags", err)
-		os.Exit(1)
+		log.Fatal("failed to parse go test flags:", err)
 	}
 
-	opt, _ := option.New([]string{*Subject, "--namespace", "finch"})
-
-	ginkgo.SynchronizedBeforeSuite(func() []byte {
-		tests.SetupLocalRegistry(opt)
-		return nil
-	}, func(bytes []byte) {})
-
-	ginkgo.SynchronizedAfterSuite(func() {
-		tests.CleanupLocalRegistry(opt)
-		// clean up everything after the local registry is cleaned up
-		command.RemoveAll(opt)
-	}, func() {})
-
-	var pOpt = option.New
-	if *SubjectPrefix != "" {
-		var modifiers []option.Modifier
-		if *PrefixedSubjectEnv != "" {
-			modifiers = append(modifiers, option.Env(strings.Split(*PrefixedSubjectEnv, " ")))
-		}
-		pOpt = util.WrappedOption(strings.Split(*SubjectPrefix, " "), modifiers...)
+	opt, err := createTestOption()
+	if err != nil {
+		log.Fatal("failed to create test option:", err)
 	}
 
-	const description = "Finch Daemon Functional test"
-	ginkgo.Describe(description, func() {
-		// functional test for container APIs
-		tests.ContainerCreate(opt, pOpt)
-		tests.ContainerStart(opt)
-		tests.ContainerStop(opt)
-		tests.ContainerRestart(opt)
-		tests.ContainerRemove(opt)
-		tests.ContainerList(opt)
-		tests.ContainerRename(opt)
-		tests.ContainerStats(opt)
-		tests.ContainerAttach(opt)
-		tests.ContainerLogs(opt)
-		tests.ContainerKill(opt)
-		tests.ContainerInspect(opt)
-		tests.ContainerWait(opt)
-		tests.ContainerPause(opt)
-		tests.ContainerUnpause(opt)
+	setupTestSuite(opt)
 
-		// functional test for volume APIs
-		tests.VolumeList(opt)
-		tests.VolumeInspect(opt)
-		tests.VolumeRemove(opt)
+	pOpt := createPrefixedOption()
 
-		// functional test for network APIs
-		tests.NetworkCreate(opt, pOpt)
-		tests.NetworkRemove(opt)
-		tests.NetworkList(opt)
-		tests.NetworkInspect(opt)
-
-		// functional test for image APIs
-		tests.ImageRemove(opt)
-		tests.ImagePush(opt)
-		tests.ImagePull(opt)
-
-		// functional test for system api
-		tests.SystemVersion(opt)
-		tests.SystemEvents(opt)
-
-		// functional test for distribution api
-		tests.DistributionInspect(opt)
+	ginkgo.Describe(e2eTestDescription, func() {
+		runContainerTests(opt)
+		runVolumeTests(opt)
+		runNetworkTests(opt, pOpt)
+		runImageTests(opt)
+		runSystemTests(opt)
+		runDistributionTests(opt)
 	})
 
+	runTests(t, e2eTestDescription)
+}
+
+func createPrefixedOption() func([]string, ...option.Modifier) (*option.Option, error) {
+	if *SubjectPrefix == "" {
+		return option.New
+	}
+
+	var modifiers []option.Modifier
+	if *PrefixedSubjectEnv != "" {
+		modifiers = append(modifiers, option.Env(strings.Split(*PrefixedSubjectEnv, " ")))
+	}
+	return util.WrappedOption(strings.Split(*SubjectPrefix, " "), modifiers...)
+}
+
+func runTests(t *testing.T, description string) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, description)
+}
+
+// functional test for container APIs.
+func runContainerTests(opt *option.Option) {
+	tests.ContainerCreate(opt)
+	tests.ContainerStart(opt)
+	tests.ContainerStop(opt)
+	tests.ContainerRestart(opt)
+	tests.ContainerRemove(opt)
+	tests.ContainerList(opt)
+	tests.ContainerRename(opt)
+	tests.ContainerStats(opt)
+	tests.ContainerAttach(opt)
+	tests.ContainerLogs(opt)
+	tests.ContainerKill(opt)
+	tests.ContainerInspect(opt)
+	tests.ContainerWait(opt)
+	tests.ContainerPause(opt)
+}
+
+// functional test for volume APIs.
+func runVolumeTests(opt *option.Option) {
+	tests.VolumeList(opt)
+	tests.VolumeInspect(opt)
+	tests.VolumeRemove(opt)
+}
+
+// functional test for network APIs.
+func runNetworkTests(opt *option.Option, pOpt func([]string, ...option.Modifier) (*option.Option, error)) {
+	tests.NetworkCreate(opt, pOpt)
+	tests.NetworkRemove(opt)
+	tests.NetworkList(opt)
+	tests.NetworkInspect(opt)
+}
+
+// functional test for image APIs.
+func runImageTests(opt *option.Option) {
+	tests.ImageRemove(opt)
+	tests.ImagePush(opt)
+	tests.ImagePull(opt)
+}
+
+// .
+func runSystemTests(opt *option.Option) {
+	tests.SystemVersion(opt)
+	tests.SystemEvents(opt)
+}
+
+// functional test for distribution api.
+func runDistributionTests(opt *option.Option) {
+	tests.DistributionInspect(opt)
 }
 
 // parseTestFlags parses go test flags because pflag package ignores flags with '-test.' prefix
